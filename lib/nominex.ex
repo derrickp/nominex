37247f7db4e09feca493ec7ed4c14667a8a0a262
Derrick Plotsky <derrickplotsky@gmail.com>
2019-04-11 22:06:03 -0700

    Initial commit
    Just does "mix new" and then adds bits from gitignore.io to the
    .gitignore file.

---
+++ lib/nominex.ex
@@ -0,0 +1,18 @@
+defmodule Nominex do
+  @moduledoc """
+  Documentation for Nominex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nominex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nominex_test.exs
@@ -0,0 +1,8 @@
+defmodule NominexTest do
+  use ExUnit.Case
+  doctest Nominex
+
+  test "greets the world" do
+    assert Nominex.hello() == :world
+  end
+end
